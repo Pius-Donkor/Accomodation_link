b812c54ea0f9b2b1bdc9e8becf701fd54db87cdf
Pius <piusdonkor1@gmail.com>
2024-04-26 07:14:06 +0000

    Create SwiperArrowButtons.jsx

---
+++ src/UI/SwiperArrowButtons.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSwiper } from "swiper/react";
+
+import { MdOutlineArrowCircleLeft } from "react-icons/md";
+import { MdOutlineArrowCircleRight } from "react-icons/md";
+export default function SwiperArrowButtons() {
+  const swiper = useSwiper();
+  function swipeLeft() {
+    return swiper.slidePrev();
+  }
+  function swipeRight() {
+    return swiper.slideNext();
+  }
+  return (
+    <div className=" absolute top-[38%] z-10 flex w-[100%] justify-between  p-2 ">
+      <span role="button" onClick={() => swipeLeft()}>
+        <MdOutlineArrowCircleLeft
+          role="button"
+          className=" -z-10 rounded-full bg-[#ffffffbe] p-1 text-4xl"
+        />
+      </span>
+      <span role="button" onClick={() => swipeRight()}>
+        <MdOutlineArrowCircleRight
+          role="button"
+          className=" -z-10 rounded-full bg-[#ffffffbe] p-1 text-4xl"
+        />
+      </span>
+    </div>
+  );
+}
