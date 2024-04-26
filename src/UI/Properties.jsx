e816c82ff3a593342b22abaded7acb40c19dd504
Pius <piusdonkor1@gmail.com>
2024-04-26 07:14:00 +0000

    Create Properties.jsx

---
+++ src/UI/Properties.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropertiesCard from "./PropertiesCard";
+
+export default function Properties() {
+  return (
+    <div className=" flex w-[100%] items-center gap-12 px-14 py-20 ">
+      <PropertiesCard />
+    </div>
+  );
+}
