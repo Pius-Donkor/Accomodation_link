c8650639fd224289e4b265d55ee77a51784382dc
Pius <piusdonkor1@gmail.com>
2024-05-04 22:11:16 +0000

    Update OptionsBar.jsx

--- src/UI/OptionsBar.jsx
+++ src/UI/OptionsBar.jsx
@@ -3,16 +3,16 @@ import { IoFilterOutline } from "react-icons/io5";
 import Button from "./Button";
 export default function OptionsBar() {
   return (
-    <nav className="flex w-[100%] items-center justify-between gap-12 bg-slate-100 px-14 py-2 ">
+    <nav className="fixed  z-[999] mt-[5rem] flex  w-[100%] items-center justify-between gap-12 bg-slate-100 px-14 py-2 ">
       <div className=" flex justify-center ">
         <Button type={"greenLight"}>
           <IoFilterOutline className="mt-1 " /> filter
         </Button>
       </div>
       <div className=" flex items-center">
-        <h1 className=" text-xl font-semibold">Sort By:</h1>
+        <h1 className=" text-lg font-semibold">Sort By:</h1>
         <select
-          className="  flex justify-center gap-1 rounded-sm bg-[#9adf9a]  px-2 py-1 text-xl shadow-sm outline-none transition-all hover:bg-green-500 hover:text-slate-100"
+          className="  flex justify-center gap-1 rounded-sm bg-[#9adf9a]  px-2 py-1 text-lg shadow-sm outline-none transition-all hover:bg-green-500 hover:text-slate-100"
           name="sort"
           id="sort"
         >
