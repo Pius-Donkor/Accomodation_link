ee000c8688afe19018432f14ce19e278b14f5637
Pius <piusdonkor1@gmail.com>
2024-05-06 18:18:02 +0000

    Update Button.jsx

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ children, type }) {
   const baseStyle = " border-2 px-3 py-1 text-xl transition-all";
   const buttonStyles = {
     transparent:
-      ` border-green-600 bg-transparent hover:bg-[#88c988b7] hover:text-[white]` +
+      ` border-green-600 md:text-2xl lg:text-xl bg-transparent hover:bg-[#88c988b7] hover:text-[white]` +
       baseStyle,
     colored:
       ` border-transparent bg-green-600  text-slate-100 transition-all hover:bg-green-700` +
