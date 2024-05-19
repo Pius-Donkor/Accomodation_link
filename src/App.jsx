7c1abe8c8d3459d852cda4ba6d26af4926397e74
Pius <piusdonkor1@gmail.com>
2024-05-19 21:36:07 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import Home from "./Pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import Layout from "./UI/Layout";
+import AllUploads from "./Pages/AllUploads";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
+        <Route path="/uploads" element={<AllUploads />} />
       </Routes>
     </BrowserRouter>
   );
