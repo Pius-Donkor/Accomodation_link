f222f122c5aa3f4008ddf8ce26c522ad32fd66f7
Pius <piusdonkor1@gmail.com>
2024-05-23 05:57:43 +0000

    Update Properties.jsx

--- src/Features/properties/Properties.jsx
+++ src/Features/properties/Properties.jsx
@@ -2,26 +2,51 @@ import React, { useEffect, useState } from "react";
 import PropertiesCard from "./PropertiesCard";
 import useGetProperties from "./useGetProperties";
 import Button from "../../UI/Button";
+import { useSearchParams } from "react-router-dom";
+
+import { moneyToNumber } from "../../utils/helper";
 export default function Properties() {
   const { properties, propertiesError, isloading } = useGetProperties();
   const [currentPage, setCurrentPage] = useState(0);
+  const [searchParams] = useSearchParams();
+  const sortParameter = searchParams.get("sortBy");
+  let sortedProperties;
+
   const propertiesPerPage = 9;
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
+
   useEffect(() => {
     scrollToTop();
   }, [currentPage]);
 
-  if (isloading) return <p>loading</p>;
+  if (isloading)
+    return <p className="mt-[10rem] text-center text-[5rem] ">loading.....</p>;
+
+  if (sortParameter === "all") {
+    sortedProperties = properties.slice(0, -1);
+  } else if (sortParameter === "low-price") {
+    sortedProperties = properties
+      .slice(0, -1)
+      .sort((a, b) => moneyToNumber(a.price) - moneyToNumber(b.price));
+  } else if (sortParameter === "high-price") {
+    sortedProperties = properties
+      .slice(0, -1)
+      .sort((a, b) => moneyToNumber(b.price) - moneyToNumber(a.price));
+  } else {
+    sortedProperties = properties.slice(0, -1);
+  }
+
   // Calculate the total number of pages
   const totalPages = Math.ceil(properties.length / propertiesPerPage);
 
   // Calculate the properties to display for the current page
-  const currentProperties = properties.slice(
+  const currentProperties = sortedProperties.slice(
     currentPage * propertiesPerPage,
     currentPage * propertiesPerPage + propertiesPerPage,
   );
@@ -38,11 +63,12 @@ export default function Properties() {
       setCurrentPage(currentPage - 1);
     }
   };
+
   return (
     <div className=" mt-16 flex w-[100%] flex-col items-center justify-center gap-12 px-14 py-20">
       <div className="flex w-[100%] flex-row flex-wrap items-center justify-center gap-12 ">
         {currentProperties.map((property) => (
-          <PropertiesCard key={properties.id} property={property} />
+          <PropertiesCard key={property.id} property={property} />
         ))}
       </div>
       {/* next/prev buttons */}
