6dcf0c838d024ad3e967b86b60adbd7285efd65e
Pius <piusdonkor1@gmail.com>
2024-05-23 05:57:57 +0000

    Create FilterWindow.jsx

---
+++ src/UI/FilterWindow.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function FilterWindow() {
+  return (
+    <div>
+      <div>
+        <p>Price range :</p>
+        <input type="range" />
+      </div>
+      <div>
+        <p>rating</p>
+        <input type="range" name="" id="" />
+      </div>
+    </div>
+  );
+}
