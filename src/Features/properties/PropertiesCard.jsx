87718980d4c32df6484211a8d13c72977f15df76
Pius <piusdonkor1@gmail.com>
2024-05-28 21:14:51 +0000

    Update PropertiesCard.jsx

--- src/Features/properties/PropertiesCard.jsx
+++ src/Features/properties/PropertiesCard.jsx
@@ -92,7 +92,9 @@ export default function PropertiesCard({ property }) {
           </span>
         </span>
         <div className="self-center">
-          <Button type="transparent">View Listing</Button>
+          <Button type="transparent" link={`propertyDetails/${property.id}`}>
+            View Listing
+          </Button>
         </div>
       </div>
     </div>
