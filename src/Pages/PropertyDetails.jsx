b887cc28e709d39a97daf716d4fcece4505d0180
Pius <piusdonkor1@gmail.com>
2024-05-28 21:15:07 +0000

    Update PropertyDetails.jsx

--- src/Pages/PropertyDetails.jsx
+++ src/Pages/PropertyDetails.jsx
@@ -1,5 +1,118 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import useGetProperty from "../Features/properties/useGetProperty";
+import PageError from "../UI/PageError";
+import { useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 export default function PropertyDetails() {
-  return <div>PropertyDetails</div>;
+  const { id } = useParams();
+  const { property, propertyLoading, propertyError } = useGetProperty(id);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  if (propertyLoading)
+    return <p className=" mt-[10rem] text-7xl ">loading...</p>;
+  if (propertyError) return <PageError errorMessage={propertyError.message} />;
+
+  return (
+    <div className="mx-12 mb-[5rem] mt-[10rem] w-full ">
+      {/* image carousel and map */}
+      <section className="flex w-[100vw] ">
+        {/* CAROUSEL */}
+        <div className="w-60% relative">
+          <Swiper
+            className="mb-2 h-[30rem] w-[40rem] "
+            modules={[FreeMode, Navigation, Thumbs]}
+            loop={true}
+            spaceBetween={10}
+            navigation={true}
+            thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
+          >
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+            </SwiperSlide>
+          </Swiper>
+          <Swiper
+            modules={[FreeMode, Navigation, Thumbs]}
+            onSwiper={setThumbsSwiper}
+            loop={true}
+            spaceBetween={10}
+            slidesPerView={6}
+            freeMode={true}
+            watchSlidesProgress={true}
+            className=" w-[40rem]"
+          >
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        {/* map */}
+        <div className="w-40%"></div>
+      </section>
+    </div>
+  );
 }
