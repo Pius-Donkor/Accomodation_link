dbd94b6e12ca4fbd5c3b9c8b9fed21511bc5411e
Pius <piusdonkor1@gmail.com>
2024-05-30 06:30:52 +0000

    Update useMoveBack.js

--- src/hooks/useMoveBack.js
+++ src/hooks/useMoveBack.js
@@ -1,7 +1,5 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
-
-export default function useMoveBack() {
+export function useMoveBack() {
   const navigate = useNavigate();
   function moveBack() {
     return navigate(-1);
