90d1b8bc3d94b7115a8684f7ec1b91f14186352f
Pius <piusdonkor1@gmail.com>
2024-06-03 17:01:17 +0000

    Update Button.jsx

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -19,14 +19,14 @@ export default function Button({
       baseStyle,
     greenLight: ` flex justify-center gap-1 rounded-sm bg-[#9adf9a] px-2 py-1 ${disable ? "bg-slate-300" : ""} text-xl shadow-sm transition-all hover:bg-slate-500 hover:text-slate-100  `,
     nav: ` flex items-center rounded-2xl bg-[#edf3ff] px-2 py-1 shadow-lg transition-colors duration-300 hover:bg-[#bde2bd] `,
-    reddish: ` flex items-center rounded-2xl bg-[#b33479] px-2 py-1 shadow-lg text-[white] transition-colors duration-300  hover:bg-[#9c5f81] `,
-    transparentRed: ` flex items-center rounded-2xl bg-[transparent]  border-[#b33479] border-[1px] px-2 py-0 shadow-lg text-[#b33479] transition-colors duration-300 hover:bg-[#9c5f81]  hover:text-[white]`,
+    reddish: ` flex items-center justify-center rounded-2xl bg-[#b33479] px-2 py-1 shadow-lg text-[white] transition-colors duration-300  hover:bg-[#9c5f81] `,
+    transparentRed: ` flex items-center justify-center rounded-2xl bg-[transparent]  border-[#b33479] border-[1px] px-2 py-0 shadow-lg text-[#b33479] transition-colors duration-300 hover:bg-[#9c5f81]  hover:text-[white]`,
   };
   if (link) {
     return (
       <button
         disabled={disable}
-        onClick={() => navigate(link)}
+        onClick={() => navigate(link, { replace: true })}
         className={buttonStyles[type]}
       >
         {children}
