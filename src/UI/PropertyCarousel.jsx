46c8ed7977589cc2ac578d1a0ebddae2d5f00dbd
Pius <piusdonkor1@gmail.com>
2024-06-03 17:01:20 +0000

    Update PropertyCarousel.jsx

--- src/UI/PropertyCarousel.jsx
+++ src/UI/PropertyCarousel.jsx
@@ -27,14 +27,15 @@ export default function PropertyCarousel({
     normal: {
       overlay: "",
       cover: "",
-      swiper: "mb-2 h-[30rem] w-[40rem]",
+      swiper: "mb-2 h-[20rem] md:h-[30rem]  flex-none lg:w-[40rem] w-full  ",
       thumbs: "w-[40rem]",
     },
     zoom: {
       overlay:
         " fixed left-0 top-[5rem] z-[1000] flex h-[100dvh] w-[100vw] items-center justify-center overflow-auto bg-[#00000088] pb-[10rem] backdrop-blur-md ",
-      cover: " bg-slate-50 p-8 mt-[30rem] relative ",
-      swiper: "mb-2 w-[50rem]  ",
+      cover:
+        " bg-slate-50 lg:p-8 p-2 mt-[10rem] lg:mt-[30rem] lg:w-fit w-[100%] relative ",
+      swiper: "mb-2 w-[100%] lg:w-[50rem]  ",
       thumbs: "w-[40rem]",
     },
   };
