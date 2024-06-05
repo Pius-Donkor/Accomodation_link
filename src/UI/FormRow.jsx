b9cc2bdc98799153c859f63a68164231161a24d4
Pius <piusdonkor1@gmail.com>
2024-06-05 11:53:17 +0000

    Update FormRow.jsx

--- src/UI/FormRow.jsx
+++ src/UI/FormRow.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-export default function FormRow({ children, error, field = "", icon }) {
+export default function FormRow({
+  children,
+  error,
+  field = "",
+  icon,
+  childElement = "",
+}) {
   return (
-    <div className=" flex items-center justify-between gap-2">
-      <label htmlFor={field} className="flex items-center gap-1">
-        {icon && <icon.icon className=" text-xl text-slate-600  " />}
-        {field.slice(0, 1).toUpperCase() + field.slice(1)} :
-      </label>
-      {children}
-      {error && <p>{error}</p>}
+    <div className=" flex flex-col gap-2">
+      <div className=" w flex flex-col items-center gap-2 overflow-y-auto md:flex-row md:justify-between ">
+        {childElement !== "button" && (
+          <label htmlFor={field} className="flex items-center gap-1">
+            {icon && <icon.icon className=" text-xl text-slate-600  " />}
+            {field.slice(0, 1).toUpperCase() + field.slice(1)} :
+          </label>
+        )}
+        {children}
+      </div>
+      {error && <p className="bg-[#eebbbb] text-right text-[red] ">{error}</p>}
     </div>
   );
 }
