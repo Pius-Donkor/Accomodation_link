fd43a7453a46cd653f69228fe66570c98fd864c1
Pius <piusdonkor1@gmail.com>
2024-06-05 11:53:01 +0000

    Update AuthBackground.jsx

--- src/UI/AuthBackground.jsx
+++ src/UI/AuthBackground.jsx
@@ -6,18 +6,15 @@ export default function AuthBackground({
 }) {
   return (
     <div
-      className="flex h-[100dvh] w-full  items-center justify-center bg-contain bg-local bg-center bg-no-repeat  "
+      className="flex h-[100dvh] w-full  items-center justify-center overflow-y-auto
+       bg-contain bg-local bg-center bg-no-repeat pt-[10rem]  md:pt-2 "
       style={{
         backgroundImage:
           "url('/authImage-min.jpg') ,linear-gradient(to bottom, #dedfe3 60%, #fafeff 100%) ",
       }}
     >
-      <div className="flex h-fit w-fit flex-col items-center  rounded-2xl bg-[#fff8f8c9] px-4 pb-12 text-slate-800 shadow-md backdrop-blur-sm ">
-        <img
-          src="/webLogo.png"
-          alt="logo"
-          className="mt-[-100px] h-[250px] w-[300px] "
-        />
+      <div className="flex h-fit w-fit flex-col items-center  rounded-2xl bg-[#fff8f8c9] px-4 pb-4  text-slate-800 shadow-md backdrop-blur-sm ">
+        <img src="/webLogo.png" alt="logo" className=" h-[200px] w-[200px] " />
         <p className="mb-4 flex text-3xl ">
           <GiQuillInk /> {heading}
         </p>
