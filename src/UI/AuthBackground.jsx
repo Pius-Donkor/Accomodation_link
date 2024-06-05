869ebfeca6d183ed7acef50c9c004a9f7f4de7af
Pius <piusdonkor1@gmail.com>
2024-06-05 04:38:42 +0000

    Update AuthBackground.jsx

--- src/UI/AuthBackground.jsx
+++ src/UI/AuthBackground.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { GiQuillInk } from "react-icons/gi";
 export default function AuthBackground({
   children,
   heading = "sign-up for free",
@@ -12,9 +12,15 @@ export default function AuthBackground({
           "url('/authImage-min.jpg') ,linear-gradient(to bottom, #dedfe3 60%, #fafeff 100%) ",
       }}
     >
-      <div className="flex h-fit w-fit flex-col items-center rounded-2xl bg-[#fff8f8c9] pb-12 text-slate-800 shadow-md backdrop-blur-sm ">
-        <img src="/webLogo.png" alt="logo" className="w-[300px]" />
-        <p className="text-3xl ">{heading}</p>
+      <div className="flex h-fit w-fit flex-col items-center  rounded-2xl bg-[#fff8f8c9] px-4 pb-12 text-slate-800 shadow-md backdrop-blur-sm ">
+        <img
+          src="/webLogo.png"
+          alt="logo"
+          className="mt-[-100px] h-[250px] w-[300px] "
+        />
+        <p className="mb-4 flex text-3xl ">
+          <GiQuillInk /> {heading}
+        </p>
         {children}
       </div>
     </div>
