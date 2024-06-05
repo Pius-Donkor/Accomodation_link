971f2e16b35d2dcc76b0287434a9d6dfef0ea5ef
Pius <piusdonkor1@gmail.com>
2024-06-05 11:53:23 +0000

    Update Select.jsx

--- src/UI/Select.jsx
+++ src/UI/Select.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function Select({
   children,
-  type = "text",
   register = {},
   required = "This field is required",
   field,
@@ -10,9 +9,9 @@ export default function Select({
 }) {
   return (
     <select
+      className=" text-md min-w-[15rem] rounded-3xl bg-[#2b28289a] px-2 py-1 text-[white] shadow-md outline-none  "
       id={field}
       disabled={disabled}
-      type={type}
       {...register(field, { required: required })}
     >
       {children}
