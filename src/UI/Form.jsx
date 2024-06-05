fedcafc82d198f8e07b2c2e50af385478358d554
Pius <piusdonkor1@gmail.com>
2024-06-05 11:53:14 +0000

    Update Form.jsx

--- src/UI/Form.jsx
+++ src/UI/Form.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function Form({ children }) {
-  return <form className=" space-y-4">{children}</form>;
+export default function Form({ children, onSubmit, handleSubmit, onError }) {
+  return (
+    <form onSubmit={handleSubmit(onSubmit, onError)} className=" space-y-4">
+      {children}
+    </form>
+  );
 }
