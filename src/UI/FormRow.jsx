b05dc7e84518e36c46553722993bee4c363f6302
Pius <piusdonkor1@gmail.com>
2024-06-05 04:38:46 +0000

    Update FormRow.jsx

--- src/UI/FormRow.jsx
+++ src/UI/FormRow.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function FormRow({ children, error, field = "" }) {
+export default function FormRow({ children, error, field = "", icon }) {
   return (
-    <div>
-      <label htmlFor={field}>
+    <div className=" flex items-center justify-between gap-2">
+      <label htmlFor={field} className="flex items-center gap-1">
+        {icon && <icon.icon className=" text-xl text-slate-600  " />}
         {field.slice(0, 1).toUpperCase() + field.slice(1)} :
       </label>
       {children}
