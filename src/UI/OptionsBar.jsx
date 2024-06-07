60a7ba80f43743aac46f510160e3b3b5f669842a
Pius <piusdonkor1@gmail.com>
2024-06-07 11:00:46 +0000

    Update OptionsBar.jsx

--- src/UI/OptionsBar.jsx
+++ src/UI/OptionsBar.jsx
@@ -8,16 +8,17 @@ import SortBy from "./SortBy";
 import Modal from "./Modal";
 import FilterWindow from "./FilterWindow";
 import useFilterSort from "../hooks/useFilterSort";
-export default function OptionsBar() {
+export default function OptionsBar({ page = "home" }) {
   const [navFoldOpen, setNavFoldOpen] = useState(false);
   const { sortedProperties } = useFilterSort();
   // checking for small mobile screens
-  const isSmallScren = useRef(window.innerWidth).current < 768;
-  const toggleNavFoldOpening = navFoldOpen && isSmallScren;
+  const isSmallScreen = useRef(window.innerWidth).current < 768;
+
+  const toggleNavFoldOpening = navFoldOpen && isSmallScreen;
 
   return (
     <nav
-      className={` fixed z-[999] w-[100%] transition-all ${toggleNavFoldOpening ? "mt-[6.05rem] " : "mt-[-5.5rem]"} flex  flex-col items-center gap-10 px-14 py-2 shadow-md   backdrop-blur-lg md:mt-20
+      className={` ${page === "home" ? "fixed md:mt-20 " : "absolute  "} z-[999] w-[100%] transition-all ${toggleNavFoldOpening ? "mt-[6.05rem] " : `${isSmallScreen && "mt-[-5.5rem]"}`} flex  flex-col items-center gap-10 px-14 py-2 shadow-md   backdrop-blur-lg 
     md:w-[100%] md:flex-row md:items-center md:justify-between md:gap-12 md:bg-slate-100  `}
     >
       <div className=" flex justify-center ">
