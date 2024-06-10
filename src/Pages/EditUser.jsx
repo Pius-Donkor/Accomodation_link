94f2513391e6a3e77ef0049930fff0873bf8b4fd
Pius <piusdonkor1@gmail.com>
2024-06-10 14:26:47 +0000

    Update EditUser.jsx

--- src/Pages/EditUser.jsx
+++ src/Pages/EditUser.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDisplayOptionsBar } from "../contexts/DisplayOptionsContext";
+import SignUpForm from "../Features/authentication/SignUpForm";
 
 export default function EditUser() {
-  return <div>EditUser</div>;
+  const { setDisplayOptionsBar } = useDisplayOptionsBar();
+  useEffect(() => setDisplayOptionsBar(false));
+  return (
+    <div className="mt-4 flex h-full w-full items-center justify-center overflow-y-auto px-4 ">
+      <SignUpForm id={"1"} />
+    </div>
+  );
 }
