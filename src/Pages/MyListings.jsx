4405aa39b9170d7437071c95d616bbc369148c6b
Pius <piusdonkor1@gmail.com>
2024-06-10 14:26:54 +0000

    Update MyListings.jsx

--- src/Pages/MyListings.jsx
+++ src/Pages/MyListings.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDisplayOptionsBar } from "../contexts/DisplayOptionsContext";
+import Properties from "../Features/properties/Properties";
+import PropertiesCard from "../Features/properties/PropertiesCard";
+import Button from "../UI/Button";
+import { MdAddHome } from "react-icons/md";
+import Modal from "../UI/Modal";
+import CreateEditListing from "../Features/UserListings/CreateEditListing";
+
+const Listings = Properties(PropertiesCard);
 
 export default function MyListings() {
-  return <div></div>;
+  const { setDisplayOptionsBar } = useDisplayOptionsBar();
+  useEffect(() => setDisplayOptionsBar(true));
+  return (
+    <section className="relative mt-8 w-full ">
+      <div className="absolute right-4 top-[-4rem]">
+        <Modal>
+          <Modal.Open isButton={true} openName={"create_listing"}>
+            <Button>
+              <MdAddHome /> create new listing
+            </Button>
+          </Modal.Open>
+          <Modal.Window openName={"create_listing"}>
+            <CreateEditListing />
+          </Modal.Window>
+        </Modal>
+      </div>
+      <div className=" flex w-full px-4">
+        <div className=" mx-8 h-[80dvh] w-full  overflow-y-auto rounded-lg bg-slate-700 pt-8  ">
+          <Listings allowCrud={true} noPadding={true} />
+        </div>
+      </div>
+    </section>
+  );
 }
