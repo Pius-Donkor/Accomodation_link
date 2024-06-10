461bd2a6e98983ac55aeb8d032e6b3d5a59a416f
Pius <piusdonkor1@gmail.com>
2024-06-10 14:27:26 +0000

    Update TextArea.jsx

--- src/UI/TextArea.jsx
+++ src/UI/TextArea.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
 export default function TextArea({
-  register = {},
+  register = () => {},
   required = "This field is required",
   field,
   disabled = false,
-  children,
 }) {
   return (
     <textarea
+      className=" text-md min-h-20 min-w-[15rem] rounded-xl bg-[#2b28289a] px-2 py-1 text-[white] shadow-md outline-none  "
       id={field}
       disabled={disabled}
       {...register(field, { required: required })}
-    >
-      {children}
-    </textarea>
+    />
   );
 }
