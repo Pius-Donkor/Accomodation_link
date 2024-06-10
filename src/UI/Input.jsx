7e92fcdea9e2780c550cb25d6513390fe17f8dac
Pius <piusdonkor1@gmail.com>
2024-06-10 14:27:03 +0000

    Update Input.jsx

--- src/UI/Input.jsx
+++ src/UI/Input.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Input({
   type = "text",
-  register = {},
+  register = () => {},
   required = "This field is required",
   field,
   disabled = false,
@@ -11,7 +11,7 @@ export default function Input({
 }) {
   return (
     <input
-      className=" text-md min-w-[15rem] rounded-3xl bg-[#2b28289a] px-2 py-1 text-[white] shadow-md outline-none"
+      className={` ${type !== "file" ? "text-md min-w-[15rem] rounded-3xl bg-[#2b28289a] px-2 py-1 text-[white] shadow-md outline-none" : " bg-blue-500"} `}
       id={field}
       disabled={disabled}
       type={type}
