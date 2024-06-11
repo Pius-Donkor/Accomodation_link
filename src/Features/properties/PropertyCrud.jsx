ddfae02beab47ef2b0aeee3516dd78cce7ebe052
Pius <piusdonkor1@gmail.com>
2024-06-11 04:21:27 +0000

    Update PropertyCrud.jsx

--- src/Features/properties/PropertyCrud.jsx
+++ src/Features/properties/PropertyCrud.jsx
@@ -4,13 +4,16 @@ import { MdModeEditOutline } from "react-icons/md";
 import { PiDotsThreeOutlineVerticalBold } from "react-icons/pi";
 import useOutsideClick from "../../hooks/useOutsideClick";
 import Button from "../../UI/Button";
-export default function PropertyCrud() {
+import CrudButton from "../../UI/CrudButton";
+import Modal from "../../UI/Modal";
+import CreateEditListing from "../UserListings/CreateEditListing";
+export default function PropertyCrud({ property }) {
   const [displayCrud, setDisplayCrud] = useState(false);
   const closeCrud = () => setDisplayCrud(false);
   function handleDisplayCrud() {
     setDisplayCrud((prev) => !prev);
   }
-  const { ref } = useOutsideClick(closeCrud);
+  // const { ref } = useOutsideClick(closeCrud);
   return (
     <div className="relative">
       <Button type="transparent" onClick={handleDisplayCrud}>
@@ -18,22 +21,17 @@ export default function PropertyCrud() {
       </Button>
 
       {displayCrud && (
-        <div
-          ref={ref}
-          className=" absolute right-[-5rem] top-[-0.5rem] z-20 flex w-fit flex-col rounded-sm border border-[#00000046] bg-slate-50 shadow-2xl "
-        >
-          <span
-            role="button"
-            className="ho flex items-center justify-center gap-1 border-b-2 border-[#00000046] bg-slate-100 p-1 transition-colors duration-300 hover:bg-slate-300"
-          >
-            <MdDelete className="text-lg" /> Delete
-          </span>
-          <span
-            role="button"
-            className="flex items-center justify-center gap-1 border-b-2 border-[#00000046] bg-slate-100 p-1 transition-colors duration-300 hover:bg-slate-300"
-          >
-            <MdModeEditOutline className="text-lg" /> Edit
-          </span>
+        <div className=" absolute right-[-5rem] top-[-0.5rem] z-20 flex w-fit flex-col rounded-sm border border-[#00000046] bg-slate-50 shadow-2xl ">
+          <Modal>
+            <Modal.Open openName={"edit"}>
+              <CrudButton Icon={MdModeEditOutline}>Edit</CrudButton>
+            </Modal.Open>
+            <Modal.Window openName={"edit"}>
+              <CreateEditListing property={property} id={property?.id} />
+            </Modal.Window>
+
+            <CrudButton Icon={MdDelete}>Delete</CrudButton>
+          </Modal>
         </div>
       )}
     </div>
