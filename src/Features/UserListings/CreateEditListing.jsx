f48e99b7f1a4f668006c357587188ecbac32c076
Pius <piusdonkor1@gmail.com>
2024-06-11 04:21:29 +0000

    Update CreateEditListing.jsx

--- src/Features/UserListings/CreateEditListing.jsx
+++ src/Features/UserListings/CreateEditListing.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import CreateEditListingForm from "./CreateEditListingForm";
 
-export default function CreateEditListing() {
+export default function CreateEditListing({ property, id }) {
   return (
     <div className="  h-[100dvh] w-full overflow-y-auto bg-slate-200 px-6  pb-8 pt-12 shadow-xl md:h-[40rem] md:w-fit ">
       <div className="mb-12 flex items-center text-xl font-semibold">
         <img className="w-20" src="/houseIcon.png" alt="house_icon" />
-        <p>Enter your property details</p>
+        <p> {id ? "Edit" : "Enter"} your property details</p>
       </div>
-      <CreateEditListingForm />
+      <CreateEditListingForm property={property} id={id} />
     </div>
   );
 }
