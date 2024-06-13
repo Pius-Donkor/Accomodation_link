cd17b73328628a3e624a8952fd2bbf00ad379e0c
Pius <piusdonkor1@gmail.com>
2024-06-13 23:58:54 +0000

    Update LoadingProperties.jsx

--- src/Features/properties/LoadingProperties.jsx
+++ src/Features/properties/LoadingProperties.jsx
@@ -6,7 +6,7 @@ export default function LoadingProperties() {
     <div className=" mt-16 flex w-[100%] flex-col items-center justify-center gap-12 px-14 py-20">
       <div className="flex w-[100%] flex-row flex-wrap items-center justify-center gap-12 ">
         {Array.from({ length: 6 }, (_, i) => (
-          <LoadingPropertyCard />
+          <LoadingPropertyCard key={i} />
         ))}
       </div>
     </div>
