54454e41414a446965ee1a9db39f2c7b74d6d920
Pius <piusdonkor1@gmail.com>
2024-06-15 21:55:06 +0000

    Update EditUser.jsx

--- src/Pages/EditUser.jsx
+++ src/Pages/EditUser.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect } from "react";
 import { useDisplayOptionsBar } from "../contexts/DisplayOptionsContext";
 import SignUpForm from "../Features/authentication/SignUpForm";
+import useGetUser from "../Features/User/useGetUser";
 
 export default function EditUser() {
   const { setDisplayOptionsBar } = useDisplayOptionsBar();
+  const { error, isLoading, userData } = useGetUser();
   useEffect(() => setDisplayOptionsBar(false));
+  if (isLoading) return <p>Loading...</p>;
+
   return (
-    <div className="mt-4 flex h-full w-full items-center justify-center overflow-y-auto px-4 ">
-      <SignUpForm id={"1"} />
+    <div className="mt-4 flex h-full  items-center justify-center overflow-y-auto  ">
+      <div className="w-fit rounded-md bg-slate-300  p-4  shadow-md  ">
+        <SignUpForm user={userData} userId={userData?.userId} />
+      </div>
     </div>
   );
 }
