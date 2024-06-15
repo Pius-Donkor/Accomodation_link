b847153cc2d3c76bca3ae43381f2596b3aaf1ffe
Pius <piusdonkor1@gmail.com>
2024-06-15 21:55:00 +0000

    Create useGetAuthUser.js

---
+++ src/Features/User/useGetAuthUser.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { auth } from "../../Services/firebase";
+
+export default function useGetAuthUser() {
+  const [authUserId, setAuthUserId] = useState("loading");
+  const [isUserVerified, setIsUserVerified] = useState(false);
+  const [isUser, setIsUser] = useState(null);
+  const [isLoading, setIsLoading] = useState("loading");
+  useEffect(() => {
+    setIsLoading("loading");
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuthUserId(user.uid);
+        setIsUser(user !== null || user !== undefined);
+        setIsUserVerified(user.emailVerified);
+        setIsLoading("finish");
+      } else {
+        setIsLoading("finish");
+        setAuthUserId(null);
+      }
+    });
+
+    // Clean up the subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  return { authUserId, isUserVerified, isUser, isLoading };
+}
