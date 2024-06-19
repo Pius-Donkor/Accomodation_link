ebd2e7945ff561e67e0fda82831bd58ef85ee67c
Pius <piusdonkor1@gmail.com>
2024-06-19 20:42:56 +0000

    Create useGetRatings.js

---
+++ src/Features/Ratings/useGetRatings.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getRatings } from "../../Services/apiRatings";
+export default function useGetRatings(id) {
+  const {
+    data: ratings = [],
+    isLoading: ratingsLoading,
+    error: ratingsError,
+  } = useQuery({
+    queryFn: () => getRatings(id),
+    queryKey: ["ratings", id],
+  });
+
+  return { ratings, ratingsLoading, ratingsError };
+}
