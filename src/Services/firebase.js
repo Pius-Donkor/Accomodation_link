ba23218e9f58d23c6d8c8c630772274f889c3ca6
Pius <piusdonkor1@gmail.com>
2024-06-22 21:32:14 +0000

    Update firebase.js

--- src/Services/firebase.js
+++ src/Services/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,6 +22,7 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const database = getDatabase(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 // console.log("helllooo");
