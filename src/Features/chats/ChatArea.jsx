67c759e75ebce99f8c4d6984367ba9d7c63e6601
Pius <piusdonkor1@gmail.com>
2024-06-24 11:57:07 +0000

    Update ChatArea.jsx

--- src/Features/chats/ChatArea.jsx
+++ src/Features/chats/ChatArea.jsx
@@ -1,11 +1,34 @@
-import React from "react";
-
-export default function ChatArea({ ownerName, activeChatId, children }) {
+import React, { useState } from "react";
+import Button from "../../UI/Button";
+import useSendUpdateMessage from "./useSendUpdateMessage";
+import toast from "react-hot-toast";
+export default function ChatArea({
+  chatParticipantName,
+  activeChatId,
+  children,
+  senderId,
+}) {
+  const [message, setMessage] = useState();
+  const { isSending, messageError, sendUpdateMessage } = useSendUpdateMessage();
+  function handleSendUpdateMessage() {
+    sendUpdateMessage(
+      {
+        senderId: senderId,
+        chatId: activeChatId,
+        content: message,
+      },
+      {
+        onSuccess: () => {
+          toast.success("message sent successfully");
+        },
+      },
+    );
+  }
   return (
     <div className="flex w-1/2 flex-col justify-between bg-white">
       {/* Chat header */}
       <div className="flex items-center justify-between border-b bg-slate-50 p-5  shadow-md">
-        <h2>{ownerName || "loading..."}</h2>
+        <h2>{chatParticipantName || "loading..."}</h2>
         {/* Status indicator here if needed */}
       </div>
 
@@ -16,12 +39,14 @@ export default function ChatArea({ ownerName, activeChatId, children }) {
       </ul>
 
       {/* Message input */}
-      <div className="border-t p-5">
+      <div className="flex items-center border-t p-5 ">
         <input
+          onChange={(e) => setMessage(e.target.value)}
           type="text"
           placeholder="Type a message..."
-          className="w-full border p-2"
+          className="w-full rounded-md border border-green-300 p-2 outline-none   "
         />
+        <Button onClick={handleSendUpdateMessage} disable={isSending}>send</Button>
       </div>
     </div>
   );
