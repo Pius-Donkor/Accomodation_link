5200ce3eb0b049ca789d394342afa8e2b3d31418
Pius <piusdonkor1@gmail.com>
2024-06-24 03:09:35 +0000

    Update useGetOwner.js

--- src/Features/User/useGetOwner.js
+++ src/Features/User/useGetOwner.js
@@ -1,8 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getPropertyOwner } from "../../Services/apiUser";
 
 export default function useGetOwner(id) {
+  console.log(id);
   const {
     data: propertyOwner = {},
     isLoadingOwner,
@@ -11,7 +12,7 @@ export default function useGetOwner(id) {
     queryFn: () => getPropertyOwner(id),
     queryKey: ["user", id],
   });
-
+  console.log(propertyOwner);
   // console.log(authUserId);
   // console.log(userData, isLoadingOwner);
   return { propertyOwner, isLoadingOwner, errorOwner };
