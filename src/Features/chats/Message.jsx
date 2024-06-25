93aba2db3ba4bb480156ce40dd084c0fa7cc0efb
Pius <piusdonkor1@gmail.com>
2024-06-25 13:44:00 +0000

    Update Message.jsx

--- src/Features/chats/Message.jsx
+++ src/Features/chats/Message.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
-export default function Message() {
+export default function Message({ messageData, currentUserId }) {
+  const { senderId, content, timestamp } = messageData;
   return (
     <>
-      <li className="flex justify-end">
-        <span className="inline-block rounded bg-green-600 px-4 py-2 text-white">
-          Message content...
-        </span>
-      </li>
-      {/* Use `justify-start` for received messages */}
-      <li className="flex justify-start">
-        <span className="inline-block rounded bg-gray-300 px-4 py-2 text-black">
-          Message content...
-        </span>
-      </li>
+      {senderId === currentUserId ? (
+        <li className="flex justify-end ">
+          <span className="inline-block rounded bg-green-600 px-4 py-2 text-white">
+            {content}
+          </span>
+        </li>
+      ) : (
+        //  Use `justify-start` for received messages
+        <li className="flex justify-start">
+          <span className="inline-block rounded bg-gray-300 px-4 py-2 text-black">
+            {content}
+          </span>
+        </li>
+      )}
     </>
   );
 }
