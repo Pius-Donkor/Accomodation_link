f02eab2c87bc07d96aa62ddda52e15fe76671541
Pius <piusdonkor1@gmail.com>
2024-06-26 12:29:07 +0000

    Create ModalLittle.jsx

---
+++ src/UI/ModalLittle.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { PiDotsThreeBold } from "react-icons/pi";
+import useOutsideClick from "../hooks/useOutsideClick";
+
+export default function ModalLittle({ children, buttonBg = "whitish" }) {
+  const [displayChild, setDisplayChild] = useState(false);
+  const { ref } = useOutsideClick(() => setDisplayChild(false));
+  const buttonBgType = {
+    whitish: " bg-transparent border-[2px] border-slate-500",
+    greenish: " bg-green-300  ",
+  };
+
+  function handleOPenCloseModal() {
+    setDisplayChild((prev) => !prev);
+  }
+  return (
+    <div className="relative">
+      <button
+        className={` ${buttonBgType[buttonBg]} rounded-lg  px-[1px] py-0 `}
+        onClick={handleOPenCloseModal}
+      >
+        <PiDotsThreeBold className=" text-xl text-black" />
+      </button>
+
+      {displayChild && (
+        <div
+          ref={ref}
+          className=" absolute left-[-2rem] top-[-0.5rem] z-20 flex w-fit flex-col rounded-sm border border-[#00000046] bg-slate-50 shadow-2xl "
+        >
+          {children}
+        </div>
+      )}
+    </div>
+  );
+}
