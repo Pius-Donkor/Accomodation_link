f3507aadc26749c97014cf371a104c4827246cf0
Pius <piusdonkor1@gmail.com>
2024-07-18 12:11:00 +0000

    Update HomeBack.jsx

--- src/UI/HomeBack.jsx
+++ src/UI/HomeBack.jsx
@@ -4,12 +4,16 @@ import Button from "./Button";
 import { FaHome } from "react-icons/fa";
 import { useMoveBack } from "../hooks/useMoveBack";
 
-export default function HomeBack({ topCorner = false, isColumn = false }) {
+export default function HomeBack({
+  topCorner = false,
+  isColumn = false,
+  isRow = false,
+}) {
   const moveBack = useMoveBack();
 
   return (
     <div
-      className={`${topCorner ? " absolute left-4  top-4" : ""} inline-flex gap-8 ${isColumn && "flex-col lg:flex-row "}`}
+      className={`${topCorner ? " absolute left-4  top-4" : ""} inline-flex gap-4  ${isColumn && !isRow && "flex-col lg:flex-row "} ${isRow && !isColumn && "flex-row lg:flex-col "} `}
     >
       <Button link={"/"} type="nav">
         <FaHome className=" mr-2 text-slate-700 " /> home
