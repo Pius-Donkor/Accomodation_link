c61e6f4b9b2f384b2f9be7fcd43040ea09aaa593
Pius <piusdonkor1@gmail.com>
2024-07-23 04:31:32 +0000

    Update ChatSideBar.jsx

--- src/Features/chats/ChatSideBar.jsx
+++ src/Features/chats/ChatSideBar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-export default function ChatSideBar({ children, setSearchConversations }) {
+export default function ChatSideBar({
+  children,
+  setSearchConversations,
+  chatSidebarInView,
+}) {
   return (
-    <div className="w-1/4 border-r-2 border-r-slate-300 bg-slate-100 p-5">
+    <div
+      className={`absolute z-40 h-[100dvh] w-[100vw] border-r-2 border-r-slate-300 bg-slate-100  p-5  ${chatSidebarInView ? "md:left-0" : "left-[-100vw] md:left-[-70vw] "} transition-all duration-300 md:w-[70%] lg:relative lg:left-[none] lg:w-1/4 `}
+    >
       <div className="flex items-center space-x-2 border-b pb-5">
         <input
           type="text"
