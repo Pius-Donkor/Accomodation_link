33d395e7ef5179a922b72746ddf281f80610a5e5
Pius <piusdonkor1@gmail.com>
2024-07-24 01:12:49 +0000

    Update ChatSideBar.jsx

--- src/Features/chats/ChatSideBar.jsx
+++ src/Features/chats/ChatSideBar.jsx
@@ -7,7 +7,7 @@ export default function ChatSideBar({
 }) {
   return (
     <div
-      className={`absolute z-40 h-[100dvh] w-[100vw] border-r-2 border-r-slate-300 bg-slate-100  p-5  ${chatSidebarInView ? "md:left-0" : "left-[-100vw] md:left-[-70vw] "} transition-all duration-300 md:w-[70%] lg:relative lg:left-[none] lg:w-1/4 `}
+      className={`absolute z-40 h-[100dvh] w-[100vw] border-r-2 border-r-slate-300 bg-slate-100  p-5  ${chatSidebarInView ? "md:left-0" : "left-[-100vw] md:left-[-70vw] "} transition-all duration-300 md:w-[70%] lg:relative lg:left-0 lg:w-1/4 `}
     >
       <div className="flex items-center space-x-2 border-b pb-5">
         <input
