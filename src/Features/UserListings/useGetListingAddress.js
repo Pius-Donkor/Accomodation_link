8605dff7f06e5e1098e991b61861d5a37fe507e2
Pius <piusdonkor1@gmail.com>
2024-07-26 00:01:50 +0000

    Update useGetListingAddress.js

--- src/Features/UserListings/useGetListingAddress.js
+++ src/Features/UserListings/useGetListingAddress.js
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
 import { getAddress } from "../../Services/apiGeocoding";
 
 export default function useGetListingAddress(position) {
   const { data, error, isLoading } = useQuery({
     queryKey: ["address"],
     queryFn: () => getAddress(position),
+    enabled: position.latitude !== null && position.latitude !== null,
   });
 
   return { data, isLoading, error };
