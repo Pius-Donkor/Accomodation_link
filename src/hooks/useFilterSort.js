24fe1497cd0d2c16441b6d3f81d3a6b97bc078ce
Pius <piusdonkor1@gmail.com>
2024-08-05 19:31:12 +0000

    Update useFilterSort.js

--- src/hooks/useFilterSort.js
+++ src/hooks/useFilterSort.js
@@ -3,9 +3,12 @@ import { useSearchParams } from "react-router-dom";
 import { useFilterContext } from "./FilterState";
 import { moneyToNumber } from "../utils/helper";
 import useGetUser from "../Features/User/useGetUser";
+import useGetAllRatings from "../Features/Ratings/useGetAllRatings";
+import { useEffect, useState } from "react";
 
 export default function useFilterSort(isUser) {
   const { properties, propertiesError, isLoading } = useGetProperties();
+  const { allRatings } = useGetAllRatings();
   // const [fromUser, setFromUser] = useState(null);
   const { userData } = useGetUser();
   const [searchParams] = useSearchParams();
@@ -13,26 +16,56 @@ export default function useFilterSort(isUser) {
   const {
     state: { priceRange, rating, rentType },
   } = useFilterContext();
+  const [newProperties, setNewProperties] = useState([]);
+  useEffect(() => {
+    if (!properties.length && !allRatings.length) return;
+
+    const ratedProperties = properties.slice().map((property) => {
+      let ratings = allRatings.filter(
+        (rating) => rating.propertyId === property.id,
+      );
+
+      let averageRating =
+        ratings.reduce((sum, rating) => (sum += rating.rate), 0) /
+          ratings.length || 0;
+      return { ...property, averageRating };
+    });
+    setNewProperties(ratedProperties);
+  }, [allRatings]);
+
   let propertiesType = isUser
-    ? properties
+    ? newProperties
         .slice()
         .filter((property) => property?.userId === userData?.userId) || []
-    : properties;
+    : newProperties;
   let filteredProperties;
   let sortedProperties;
+  // add ratings to the properties
 
   // useEffect(() => {
   //   setFromUser(isUser);
   // }, [isUser]);
-  if (priceRange.min || priceRange.max || rating.min || rating.max) {
+  if (
+    priceRange.min ||
+    priceRange.max ||
+    rating.min ||
+    rating.max ||
+    rentType
+  ) {
     filteredProperties = propertiesType.slice().filter((property) => {
       const priceInRange =
         (!priceRange.min || moneyToNumber(property.price) >= priceRange.min) &&
         (!priceRange.max || moneyToNumber(property.price) <= priceRange.max);
       const ratingInRange =
-        (!rating.min || property.rating >= rating.min) &&
-        (!rating.max || property.rating <= rating.max);
-      return priceInRange && ratingInRange;
+        (!rating.min || property.averageRating >= rating.min) &&
+        (!rating.max || property.averageRating <= rating.max);
+      let rentTypeIn;
+      if (rentType === "any") rentTypeIn = property.rentType;
+      if (rentType === "home rent") rentTypeIn = property.rentType === rentType;
+      if (rentType === "hostel rent")
+        rentTypeIn = property.rentType === rentType;
+
+      return priceInRange && ratingInRange && rentTypeIn;
     });
   } else {
     filteredProperties = propertiesType.slice();
@@ -48,6 +81,10 @@ export default function useFilterSort(isUser) {
     sortedProperties = filteredProperties
       .slice()
       .sort((a, b) => moneyToNumber(b.price) - moneyToNumber(a.price));
+  } else if (sortParameter === "recent") {
+    sortedProperties = filteredProperties
+      .slice()
+      .sort((a, b) => a.date - b.date);
   } else {
     sortedProperties = filteredProperties.slice();
   }
