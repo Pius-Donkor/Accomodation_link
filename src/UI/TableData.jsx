8d81947a16fd58c4579506f912d2b95fba6a9a6d
Pius <piusdonkor1@gmail.com>
2024-08-05 03:17:33 +0000

    Update TableData.jsx

--- src/UI/TableData.jsx
+++ src/UI/TableData.jsx
@@ -4,10 +4,11 @@ export default function TableData({
   children,
   hasButtons = false,
   withFlex = false,
+  hasBoldText = false,
 }) {
   return (
     <td
-      className={` ${withFlex ? "flex" : ""}  ${hasButtons ? "justify-center gap-2" : "flex-col items-center"} whitespace-nowrap  px-6 py-4 text-center text-sm font-medium text-gray-900`}
+      className={` ${withFlex ? "flex" : ""} ${hasBoldText ? "font-medium " : ""}  ${hasButtons ? "justify-center gap-2" : "flex-col items-center"} whitespace-nowrap  px-6 py-4 text-center text-sm  text-gray-900`}
     >
       {children}
     </td>
