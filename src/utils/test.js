0e7d1b4d3881ab68f8695631072a9a9bdb4fd43d
Pius <piusdonkor1@gmail.com>
2024-08-05 19:31:31 +0000

    Update test.js

--- src/utils/test.js
+++ src/utils/test.js
@@ -1,2 +1,5 @@
-const d = new Date("2024-08-25");
-console.log(d.getTime());
+// const d = new Date("2024-08-25");
+// console.log(d.getTime());
+
+const num = Math.floor(Math.random() * 2);
+console.log(num);
