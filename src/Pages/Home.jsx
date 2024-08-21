0575bac34f0c51271d32b006e2875768355be2a8
Pius <piusdonkor1@gmail.com>
2024-08-21 11:07:43 +0000

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <OptionsBar />
-      <Properties allowCrud={false} />
+      <Properties isHomepage={true} allowCrud={false} />
     </div>
   );
 }
