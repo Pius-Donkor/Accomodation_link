c27936174342567dd7b2e3c63e351f3226e4e4ca
Pius <piusdonkor1@gmail.com>
2024-08-22 10:28:43 +0000

    Update PropertiesCard.jsx

--- src/Features/Dashboard/PropertiesCard.jsx
+++ src/Features/Dashboard/PropertiesCard.jsx
@@ -2,9 +2,27 @@ import React from "react";
 import TableRow from "../../UI/TableRow";
 import TableData from "../../UI/TableData";
 import Button from "../../UI/Button";
+import useUpdateProperties from "./useUpdateProperties";
+import toast from "react-hot-toast";
 
 export default function PropertiesCard({ property }) {
-  const { name, price, status, rentStatus, location, image } = property;
+  const { isUpdateError, isUpdating, updateProperty } = useUpdateProperties();
+  const { name, price, status, rentStatus, location, image, id } = property;
+
+  function handleUpdateProperty(task, messageType) {
+    console.log("working");
+    updateProperty(
+      {
+        ...task,
+        id,
+      },
+      {
+        onSuccess: () => {
+          toast.success(`property has been ${messageType} successfully`);
+        },
+      },
+    );
+  }
   return (
     <TableRow>
       <TableData withFlex={true}>
@@ -18,9 +36,60 @@ export default function PropertiesCard({ property }) {
       <TableData>{status}</TableData>
       <TableData>{rentStatus}</TableData>
       <TableData withFlex={true} hasButtons={true}>
-        <Button type="blue">Approve</Button>
-        <Button type="reddish">Reject</Button>
-        <Button type="green">Reset Rent</Button>
+        {status === "pending" ? (
+          <>
+            <Button
+              onclick={() =>
+                handleUpdateProperty({ status: "rejected" }, "rejected")
+              }
+              type="reddish"
+            >
+              Reject
+            </Button>
+            <Button
+              onclick={() =>
+                handleUpdateProperty({ status: "accepted" }, "approved")
+              }
+              type="blue"
+            >
+              Approve
+            </Button>
+          </>
+        ) : (
+          (status === "rejected" && (
+            <Button
+              onclick={() =>
+                handleUpdateProperty({ status: "accepted" }, "approved")
+              }
+              type="blue"
+            >
+              Approve
+            </Button>
+          )) ||
+          (status === "accepted" && (
+            <Button
+              onclick={() =>
+                handleUpdateProperty({ status: "rejected" }, "rejected")
+              }
+              type="reddish"
+            >
+              Reject
+            </Button>
+          ))
+        )}
+        <Button
+          onclick={() =>
+            handleUpdateProperty(
+              {
+                rentStatus: rentStatus === "available" ? "rented" : "available",
+              },
+              `set to ${rentStatus === "available" ? "rented" : "available"}`,
+            )
+          }
+          type="green"
+        >
+          Set Rental status
+        </Button>
       </TableData>
     </TableRow>
   );
