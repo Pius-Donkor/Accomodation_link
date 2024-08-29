bb3b51ba133734e44e25b794b08a68d4a087aca5
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-08-29 01:05:56 -0700

    Update ModalLittle.jsx

--- src/UI/ModalLittle.jsx
+++ src/UI/ModalLittle.jsx
@@ -1,37 +1,16 @@
-import React, { cloneElement, useState } from "react";
-import { PiDotsThreeBold } from "react-icons/pi";
-import useOutsideClick from "../hooks/useOutsideClick";
+import { cloneElement, createContext, useState } from "react";
 
-export default function ModalLittle({ children, buttonBg = "whitish" }) {
-  const [displayChild, setDisplayChild] = useState(false);
-  const { ref } = useOutsideClick(() => setDisplayChild(false));
-  const buttonBgType = {
-    whitish: " bg-transparent border-[2px] border-slate-500",
-    greenish: " bg-green-300  ",
-  };
+const ModalLittleContext = createContext();
+export default function ModalLittle({ children }) {
+  const [isModalOpened, setModalOpened] = useState("");
 
-  function handleOPenCloseModal() {
-    setDisplayChild((prev) => !prev);
-  }
   return (
-    <div className="relative">
-      <button
-        className={` ${buttonBgType[buttonBg]} rounded-lg  px-[1px] py-0 `}
-        onClick={handleOPenCloseModal}
-      >
-        <PiDotsThreeBold className=" text-xl text-black" />
-      </button>
-
-      {displayChild && (
-        <div
-          ref={ref}
-          className=" absolute left-[-2rem] top-[-0.5rem] z-20 flex w-fit flex-col rounded-sm border border-[#00000046] bg-slate-50 shadow-2xl "
-        >
-          {cloneElement(children, {
-            onCloseModal: () => setDisplayChild(false),
-          })}
-        </div>
-      )}
-    </div>
+    <ModalLittleContext.Provider value={{ isModalOpened, setModalOpened }}>
+      {children}
+    </ModalLittleContext.Provider>
   );
+}
+
+function OpenModalLittle({ children, onClick = () => {} }) {
+  return <>{cloneElement(children, { onClick: () => onClick() })}</>;
 }
