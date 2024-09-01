9a2aa4750d416692e2d180f098a05eaa9560bc6d
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-01 01:24:02 +0000

    Update Users.jsx

--- src/Features/Dashboard/Users.jsx
+++ src/Features/Dashboard/Users.jsx
@@ -6,10 +6,16 @@ import TableRow from "../../UI/TableRow";
 import TableHeadData from "../../UI/TableHeadData";
 import TableBody from "../../UI/TableBody";
 import TableData from "../../UI/TableData";
+import useGetAllUsers from "../User/useGetAllUsers";
+import UserTableCard from "./UserTableCard";
 
 const Users = () => {
   const [searchTerm, setSearchTerm] = useState("");
-
+  const { allUsers, error, isLoading } = useGetAllUsers();
+  const filteredUsers =
+    allUsers?.filter((user) =>
+      user?.userName.toLowerCase().includes(searchTerm.toLowerCase()),
+    ) || [];
   return (
     <div>
       <h1 className="mb-4 text-2xl font-semibold">Users</h1>
@@ -28,21 +34,35 @@ const Users = () => {
             <TableHeadData>Name</TableHeadData>
             <TableHeadData>Email</TableHeadData>
             <TableHeadData>Role</TableHeadData>
+            <TableHeadData>Location</TableHeadData>
+            <TableHeadData>Contact</TableHeadData>
+            <TableHeadData>Status</TableHeadData>
             <TableHeadData>Actions</TableHeadData>
           </TableRow>
         </TableHeader>
         <TableBody>
           {/* Add dynamic user data here */}
-          <TableRow>
-            <TableData hasBoldText={true}>John Doe</TableData>
-            <TableData>john@example.com</TableData>
-            <TableData>Tenant</TableData>
-            <TableData>
-              <button className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700">
-                Delete
-              </button>
-            </TableData>
-          </TableRow>
+          {isLoading && (
+            <TableRow>
+              <TableData hasBoldText={true}>loading...</TableData>
+              <TableData>loading...</TableData>
+              <TableData>loading...</TableData>
+              <TableData>
+                <button className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700">
+                  loading...
+                </button>
+              </TableData>
+            </TableRow>
+          )}
+
+          {!isLoading && error && <div>error</div>}
+          {!isLoading && !error && allUsers.length ? (
+            filteredUsers.map((user) => (
+              <UserTableCard key={user.userId} user={user} />
+            ))
+          ) : (
+            <p>there are no users found</p>
+          )}
         </TableBody>
       </Table>
     </div>
