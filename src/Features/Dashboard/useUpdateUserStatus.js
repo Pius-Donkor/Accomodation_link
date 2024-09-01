a41f398df73ad90a54075e1587f9379d15511e47
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-01 01:24:11 +0000

    Create useUpdateUserStatus.js

---
+++ src/Features/Dashboard/useUpdateUserStatus.js
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateUserStatus as updateUserStatusApi } from "../../Services/apiUser";
+import toast from "react-hot-toast";
+
+export default function useUpdateUserStatus() {
+  const queryClient = useQueryClient();
+  const {
+    isPending: isUpdating,
+    isError: isUpdateError,
+    mutate: updateUserStatus,
+  } = useMutation({
+    mutationFn: (data) => updateUserStatusApi(data),
+    mutationKey: ["properties"],
+    onSuccess: () => {
+      queryClient.invalidateQueries(["properties"]);
+    },
+    onError: (error) => {
+      toast.error(
+        "Sorry could not perform action on this user, check your internet connection" +
+          error.message,
+      );
+    },
+  });
+
+  return { isUpdating, isUpdateError, updateUserStatus };
+}
