8083442d86b6eca2a84898ffadcacd8a877c2431
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-02 22:41:41 +0000

    Update TableBody.jsx

--- src/UI/TableBody.jsx
+++ src/UI/TableBody.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function TableBody({ children }) {
   return (
-    <tbody className=" divide-y divide-gray-200 bg-white ">{children}</tbody>
+    <tbody className="  divide-y divide-gray-200 bg-white ">{children}</tbody>
   );
 }
