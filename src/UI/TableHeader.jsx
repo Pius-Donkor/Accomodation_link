a1c9d72b18f7ebfe39ba105a803888faeb131f5a
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-02 22:41:49 +0000

    Update TableHeader.jsx

--- src/UI/TableHeader.jsx
+++ src/UI/TableHeader.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function TableHeader({ children }) {
-  return <thead className=" divide-y-8 divide-gray-200 ">{children}</thead>;
+  return (
+    <thead className=" sticky left-0 top-0 z-10 divide-y-8 divide-gray-200 bg-slate-50 ">
+      {children}
+    </thead>
+  );
 }
