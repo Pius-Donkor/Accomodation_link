2576aca19d80ccb59b580b08fd60f1bba9fd2c8e
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-07 10:27:05 +0000

    Update UserTableCard.jsx

--- src/Features/Dashboard/UserTableCard.jsx
+++ src/Features/Dashboard/UserTableCard.jsx
@@ -35,51 +35,53 @@ export default function UserTableCard({ user }) {
       <TableData>{user.status}</TableData>
 
       <TableData>
-        <ModalLittle>
-          <ModalLittle.OpenModalLittle openName={`status${user.userId}`}>
-            <Button disable={isUpdating} type="green">
-              Actions
-            </Button>
-          </ModalLittle.OpenModalLittle>
-          <ModalLittle.ModalLittleWindow
-            position="absolute bottom-[-7rem] right-[-3rem] z-50  "
-            openName={`status${user.userId}`}
-          >
-            <div className="flex flex-col gap-1 ">
-              <p className="rounded-sm bg-slate-100 p-1 font-bold ">
-                set the user status to :
-              </p>
-              <Button
-                disable={isUpdating}
-                onclick={() => handleUserAction("deleted")}
-                type="nav"
-              >
-                <RiUserUnfollowFill className="mr-1 text-lg" /> Deleted
+        <div className="relative">
+          <ModalLittle>
+            <ModalLittle.OpenModalLittle openName={`status${user.userId}`}>
+              <Button disable={isUpdating} type="green">
+                Actions
               </Button>
-              <Button
-                disable={isUpdating}
-                onclick={() => handleUserAction("suspended")}
-                type="nav"
-              >
-                <FaUserLock className="mr-1 text-lg" /> Suspended
-              </Button>
-              <Button
-                disable={isUpdating}
-                onclick={() => handleUserAction("inactive")}
-                type="nav"
-              >
-                <FaUserLargeSlash className="mr-1 text-lg" /> Inactive
-              </Button>
-              <Button
-                disable={isUpdating}
-                onclick={() => handleUserAction("active")}
-                type="nav"
-              >
-                <FaUserCheck className="mr-1 text-lg" /> Active
-              </Button>
-            </div>
-          </ModalLittle.ModalLittleWindow>
-        </ModalLittle>
+            </ModalLittle.OpenModalLittle>
+            <ModalLittle.ModalLittleWindow
+              position="absolute bottom-[-10rem] right-[-6rem] z-50  "
+              openName={`status${user.userId}`}
+            >
+              <div className="flex flex-col gap-1 ">
+                <p className="rounded-sm bg-slate-100 p-1 font-bold ">
+                  set the user status to :
+                </p>
+                <Button
+                  disable={isUpdating}
+                  onclick={() => handleUserAction("deleted")}
+                  type="nav"
+                >
+                  <RiUserUnfollowFill className="mr-1 text-lg" /> Deleted
+                </Button>
+                <Button
+                  disable={isUpdating}
+                  onclick={() => handleUserAction("suspended")}
+                  type="nav"
+                >
+                  <FaUserLock className="mr-1 text-lg" /> Suspended
+                </Button>
+                <Button
+                  disable={isUpdating}
+                  onclick={() => handleUserAction("inactive")}
+                  type="nav"
+                >
+                  <FaUserLargeSlash className="mr-1 text-lg" /> Inactive
+                </Button>
+                <Button
+                  disable={isUpdating}
+                  onclick={() => handleUserAction("active")}
+                  type="nav"
+                >
+                  <FaUserCheck className="mr-1 text-lg" /> Active
+                </Button>
+              </div>
+            </ModalLittle.ModalLittleWindow>
+          </ModalLittle>
+        </div>
       </TableData>
     </TableRow>
   );
