bb32330a7a7e0847ea573101ff2331ea1c6c06b4
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-11 07:11:38 +0000

    Update AdminDashboard.jsx

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import Layout from "../Features/Dashboard/lLayout";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import useGetUser from "../Features/User/useGetUser";
+import toast from "react-hot-toast";
 
 export default function AdminDashboard() {
-  return (
-    <Layout>
-      <Outlet />
-    </Layout>
-  );
+  const navigate = useNavigate();
+  const { userData, error, isLoading } = useGetUser();
+  console.log(userData);
+  useEffect(() => {
+    if (!userData?.role) return;
+    if (userData.role !== "admin") {
+      console.log(userData.role, userData.userName);
+      toast.error("Only an Administrator can have access to this page ");
+      navigate("/");
+    }
+  }, []);
+
+  if (isLoading) return <p>loading...</p>;
+
+  if (!isLoading && userData?.role === "admin")
+    return (
+      <Layout userData={userData}>
+        <Outlet />
+      </Layout>
+    );
+
+  return <div>hellooooo</div>;
 }
