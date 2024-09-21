f4d4e9a49c46d550d5712aba0f002b4e0ff9ce99
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-21 19:27:19 +0000

    Create BtnLoadingSpinner.jsx

---
+++ src/UI/BtnLoadingSpinner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BtnLoadingSpinner() {
+  return (
+    <svg
+      class=" border- h-5 w-5  animate-spin rounded-full border-[3px]  border-b-[#ffffff6c] border-l-slate-100 border-r-[#ffffff6c]  "
+      viewBox="0 0 24 24"
+    ></svg>
+  );
+}
