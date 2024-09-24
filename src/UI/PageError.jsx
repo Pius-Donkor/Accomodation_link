57861b4e637a56c27591e2c9c034493f95dd80ab
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-24 03:52:12 +0000

    Update PageError.jsx

--- src/UI/PageError.jsx
+++ src/UI/PageError.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Button from "./Button";
 
 export default function PageError({ errorMessage }) {
+  function reload() {
+    window.location.reload();
+  }
   return (
-    <div className="mt-[6rem] flex h-[90vh] w-[100%] items-center justify-center border ">
+    <div className="mt-[6rem] flex h-[90vh] w-full flex-col items-center justify-center gap-2 border ">
       <p className="rounded-md bg-[#dbb3b3] p-1 text-center text-5xl text-[#da4848] ">
         {errorMessage} 😭🤦‍♂️{" "}
       </p>
+
+      <Button onclick={reload}>Retry</Button>
     </div>
   );
 }
