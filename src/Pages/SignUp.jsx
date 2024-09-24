b66437436db4b4e2563fefb13e81b2444147f040
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-24 03:10:25 +0000

    Update SignUp.jsx

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AuthBackground from "../UI/AuthBackground";
 import SignUpForm from "../Features/authentication/SignUpForm";
+import { useNavigate } from "react-router-dom";
+import useGetAuthUser from "../Features/User/useGetAuthUser";
+import toast from "react-hot-toast";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+  const { isUser } = useGetAuthUser();
+  useEffect(() => {
+    if (isUser) {
+      toast.error(
+        "you are already logged-in, logout first before switching account",
+      );
+      navigate("/");
+    }
+  }, [isUser, navigate]);
   return (
     <AuthBackground heading="Sign-Up for free">
       <SignUpForm />
