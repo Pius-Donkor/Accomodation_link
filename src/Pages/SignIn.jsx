57fdec88e3069951372932883c1f5cbc901f7bca
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-24 03:10:22 +0000

    Update SignIn.jsx

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AuthBackground from "../UI/AuthBackground";
 import LoginForm from "../Features/authentication/LoginForm";
+import useGetAuthUser from "../Features/User/useGetAuthUser";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 export default function SignIn() {
+  const navigate = useNavigate();
+  const { isUser } = useGetAuthUser();
+  useEffect(() => {
+    if (isUser) {
+      toast.error(
+        "you are already logged-in, logout first before switching account",
+      );
+      navigate("/");
+    }
+  }, [isUser, navigate]);
   return (
     <AuthBackground heading="Login">
       <LoginForm />
