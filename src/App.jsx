84b24529240d1a516476167385163b32b300acf2
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-25 15:53:56 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import Dashboard from "./Features/Dashboard/Dashboard";
 import Users from "./Features/Dashboard/Users";
 import Properties from "./Features/Dashboard/Properties";
 import Prediction from "./Pages/Prediction";
+import { RentRequestProvider } from "./contexts/RentRequestContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,70 +32,72 @@ const queryClient = new QueryClient({
 });
 function App() {
   return (
-    <DisplayOptionsBarProvider>
-      <FilterStateProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
+    <RentRequestProvider>
+      <DisplayOptionsBarProvider>
+        <FilterStateProvider>
+          <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<Home />} />
+                  <Route
+                    path="/propertyDetails/:id"
+                    element={<PropertyDetails />}
+                  />
+                </Route>
                 <Route
-                  path="/propertyDetails/:id"
-                  element={<PropertyDetails />}
-                />
-              </Route>
-              <Route
-                path="/user"
-                element={
-                  <VerifyRoute>
-                    <User />
-                  </VerifyRoute>
-                }
-              >
-                <Route index element={<MyListings />} />
-                <Route path="mylistings" element={<MyListings />} />
-                <Route path="edituser" element={<EditUser />} />
-              </Route>
-              <Route path="/chats" element={<Chat />} />
-              <Route path="/prediction/:id" element={<Prediction />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/forgotpassword" element={<ForgotPassword />} />
-              <Route path="/login" element={<SignIn />} />
-              <Route path="/admindashboard" element={<AdminDashboard />}>
-                <Route index element={<Dashboard />} />
-                <Route path="overview" element={<Dashboard />} />
-                <Route path="users" element={<Users />} />
-                <Route path="allproperties" element={<Properties />} />
-              </Route>
-              <Route path="/uploads" element={<AllUploads />} />
-              <Route path="*" element={<p>page not found</p>} />
-            </Routes>
-          </BrowserRouter>
-          <Toaster
-            position="top-center"
-            gutter={12}
-            containerStyle={{ margin: "8px" }}
-            toastOptions={{
-              success: {
-                duration: 3000,
-              },
-              error: {
-                duration: 5000,
-              },
-              style: {
-                fontSize: "16px",
-                maxWidth: "500px",
-                padding: "16px 24px",
-                backgroundColor: "white",
-                color: "black",
-                boxShadow: "0px 5px 5px 10px #0000009c ",
-              },
-            }}
-          />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </FilterStateProvider>
-    </DisplayOptionsBarProvider>
+                  path="/user"
+                  element={
+                    <VerifyRoute>
+                      <User />
+                    </VerifyRoute>
+                  }
+                >
+                  <Route index element={<MyListings />} />
+                  <Route path="mylistings" element={<MyListings />} />
+                  <Route path="edituser" element={<EditUser />} />
+                </Route>
+                <Route path="/chats" element={<Chat />} />
+                <Route path="/prediction/:id" element={<Prediction />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/forgotpassword" element={<ForgotPassword />} />
+                <Route path="/login" element={<SignIn />} />
+                <Route path="/admindashboard" element={<AdminDashboard />}>
+                  <Route index element={<Dashboard />} />
+                  <Route path="overview" element={<Dashboard />} />
+                  <Route path="users" element={<Users />} />
+                  <Route path="allproperties" element={<Properties />} />
+                </Route>
+                <Route path="/uploads" element={<AllUploads />} />
+                <Route path="*" element={<p>page not found</p>} />
+              </Routes>
+            </BrowserRouter>
+            <Toaster
+              position="top-center"
+              gutter={12}
+              containerStyle={{ margin: "8px" }}
+              toastOptions={{
+                success: {
+                  duration: 3000,
+                },
+                error: {
+                  duration: 5000,
+                },
+                style: {
+                  fontSize: "16px",
+                  maxWidth: "500px",
+                  padding: "16px 24px",
+                  backgroundColor: "white",
+                  color: "black",
+                  boxShadow: "0px 5px 5px 10px #0000009c ",
+                },
+              }}
+            />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </FilterStateProvider>
+      </DisplayOptionsBarProvider>
+    </RentRequestProvider>
   );
 }
 
