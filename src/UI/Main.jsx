8c0461e5a405af852c35388bd36d1608e344c4ec
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-27 14:40:58 +0000

    Update Main.jsx

--- src/UI/Main.jsx
+++ src/UI/Main.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDisplayOptionsBar } from "../contexts/DisplayOptionsContext";
 import OptionsBar from "./OptionsBar";
 import HomeBack from "./HomeBack";
+import { useRentRequestContext } from "../contexts/RentRequestContext";
+import Modal from "./Modal";
+import RentRequestsContainer from "./RentRequestsContainer";
 
 export default function Main({ children }) {
+  const [openModal, setOpenModal] = useState(false);
   const { displayOptionsBar } = useDisplayOptionsBar();
+  const { userNewRentRequests } = useRentRequestContext();
+  const [tempRentRequest, setTempRentRequests] = useState([]);
+  useEffect(() => {
+    if (tempRentRequest.length) return;
+    setTempRentRequests(userNewRentRequests);
+    setOpenModal(Boolean(userNewRentRequests.length));
+  }, [userNewRentRequests, tempRentRequest]);
 
   return (
     <main className=" relative flex h-[100vh] w-full flex-col bg-slate-200  p-4 lg:w-[80%] ">
@@ -12,6 +23,25 @@ export default function Main({ children }) {
       <div className={`${displayOptionsBar ? "mt-12" : ""}  `}>
         <HomeBack isColumn={true} />
       </div>
+      {userNewRentRequests.length || openModal ? (
+        <Modal>
+          <Modal.Open
+            openName={"receivedRequests"}
+            preOpened={tempRentRequest.length}
+          >
+            <span></span>
+          </Modal.Open>
+          <Modal.Window openName={"receivedRequests"}>
+            <RentRequestsContainer
+              tempRentRequest={tempRentRequest}
+              setOpenModal={setOpenModal}
+              setTempRentRequests={setTempRentRequests}
+            />
+          </Modal.Window>
+        </Modal>
+      ) : (
+        ""
+      )}
       {children}
     </main>
   );
