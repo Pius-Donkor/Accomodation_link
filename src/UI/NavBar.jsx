2cb2eab267ce7d3dc5b7b4e2c3f17d4606b4d569
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-09-30 11:29:17 +0000

    Update NavBar.jsx

--- src/UI/NavBar.jsx
+++ src/UI/NavBar.jsx
@@ -10,20 +10,26 @@ import { useNavigate } from "react-router-dom";
 import useGetAuthUser from "../Features/User/useGetAuthUser";
 import RedDot from "./RedDot";
 import { useRentRequestContext } from "../contexts/RentRequestContext";
+import { useFilterContext } from "../hooks/FilterState";
 export default function NavBar() {
   const windowWidth = useRef(window.innerWidth);
   const isSmallScreen = windowWidth.current < 768;
   const [isSCMenuOpen, setIsSCMenuOpen] = useState(false);
   const { isUser } = useGetAuthUser();
   const { userHasRequests } = useRentRequestContext();
-  console.log(userHasRequests);
+  // console.log(userHasRequests);
+  const {
+    dispatch,
+    state: { searchedLocation },
+  } = useFilterContext();
   const navigate = useNavigate();
-  console.log(isUser);
+
+  // console.log(isUser);
   // console.log(isSmallScreen, windowWidth);
   return (
     <nav className=" fixed z-[9999] flex w-[100%]  justify-between bg-slate-200 px-2 py-2 md:items-center md:px-14 md:py-0 ">
       <Logo />
-
+      {/* search  */}
       <div className=" mt-5 flex md:mt-0 ">
         <button className="  h-[2.5rem]  justify-center rounded-l-lg  border-none bg-green-600 p-1 hover:bg-green-700 md:flex md:h-[2.5rem] md:w-16 ">
           <LiaSearchSolid className=" text-3xl text-slate-100 " />
@@ -32,8 +38,12 @@ export default function NavBar() {
           type="text"
           name="location"
           id="location"
+          value={searchedLocation}
           placeholder="enter location"
           className=" h-10 w-36 rounded-r-lg bg-[rgba(255,255,255,0.66)] px-3 py-1 text-xl outline-none transition-all hover:w-44 hover:bg-[#ffffffe1] "
+          onChange={(e) =>
+            dispatch({ type: "SET_SEARCHED_LOCATION", payload: e.target.value })
+          }
         />
       </div>
       <button
