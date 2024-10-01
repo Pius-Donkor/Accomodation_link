b4608eca23ab26028b35036fa6aec46ac6bc5d81
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-10-01 22:10:32 +0000

    Update Users.jsx

--- src/Features/Dashboard/Users.jsx
+++ src/Features/Dashboard/Users.jsx
@@ -8,10 +8,12 @@ import TableBody from "../../UI/TableBody";
 import TableData from "../../UI/TableData";
 import useGetAllUsers from "../User/useGetAllUsers";
 import UserTableCard from "./UserTableCard";
+import Modal from "../../UI/Modal";
 
 const Users = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const { allUsers, error, isLoading } = useGetAllUsers();
+  const [modalOpenName, setModalOpenName] = useState(null);
   const filteredUsers =
     allUsers?.filter((user) =>
       user?.userName.toLowerCase().includes(searchTerm.toLowerCase()),
@@ -28,43 +30,58 @@ const Users = () => {
           className="w-full rounded-lg border p-2 shadow-md outline-slate-200"
         />
       </div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHeadData>Name</TableHeadData>
-            <TableHeadData>Email</TableHeadData>
-            <TableHeadData>Role</TableHeadData>
-            <TableHeadData>Location</TableHeadData>
-            <TableHeadData>Contact</TableHeadData>
-            <TableHeadData>Status</TableHeadData>
-            <TableHeadData>Actions</TableHeadData>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {/* Add dynamic user data here */}
-          {isLoading && (
+      <Modal>
+        <Table>
+          <TableHeader>
             <TableRow>
-              <TableData hasBoldText={true}>loading...</TableData>
-              <TableData>loading...</TableData>
-              <TableData>loading...</TableData>
-              <TableData>
-                <button className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700">
-                  loading...
-                </button>
-              </TableData>
+              <TableHeadData>Name</TableHeadData>
+              <TableHeadData>Email</TableHeadData>
+              <TableHeadData>Role</TableHeadData>
+              <TableHeadData>Location</TableHeadData>
+              <TableHeadData>Contact</TableHeadData>
+              <TableHeadData>Status</TableHeadData>
+              <TableHeadData>Actions</TableHeadData>
             </TableRow>
-          )}
+          </TableHeader>
+          <TableBody>
+            {/* Add dynamic user data here */}
+            {isLoading && (
+              <TableRow>
+                <TableData hasBoldText={true}>loading...</TableData>
+                <TableData>loading...</TableData>
+                <TableData>loading...</TableData>
+                <TableData>
+                  <button className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700">
+                    loading...
+                  </button>
+                </TableData>
+              </TableRow>
+            )}
 
-          {!isLoading && error && <div>error</div>}
-          {!isLoading && !error && allUsers.length ? (
-            filteredUsers.map((user) => (
-              <UserTableCard key={user.userId} user={user} />
-            ))
-          ) : (
-            <p>there are no users found</p>
-          )}
-        </TableBody>
-      </Table>
+            {!isLoading && error && <div>error</div>}
+            {!isLoading && !error && allUsers.length ? (
+              filteredUsers.map((user) => (
+                <>
+                  <Modal.Open
+                    key={user.userId}
+                    openName={`user${user.userId}`}
+                    onExternalAction={() =>
+                      setModalOpenName(`user${user.userId}`)
+                    }
+                  >
+                    <UserTableCard user={user} />
+                  </Modal.Open>
+                </>
+              ))
+            ) : (
+              <p>there are no users found</p>
+            )}
+          </TableBody>
+        </Table>
+        <Modal.Window openName={modalOpenName}>
+          <p>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
+        </Modal.Window>
+      </Modal>
     </div>
   );
 };
