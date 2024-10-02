3ec5877ebbf9ae13d6a08441374640161b98676c
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-10-02 03:00:57 +0000

    Update TableData.jsx

--- src/UI/TableData.jsx
+++ src/UI/TableData.jsx
@@ -5,10 +5,13 @@ export default function TableData({
   hasButtons = false,
   withFlex = false,
   hasBoldText = false,
+  onClick = () => {},
+  isClickable = false,
 }) {
   return (
     <td
-      className={` ${withFlex ? "flex" : ""} ${hasBoldText ? "font-medium " : ""}  ${hasButtons ? "justify-center gap-2" : "flex-col items-center"}    whitespace-nowrap px-6 py-4 text-center  text-sm text-gray-900`}
+      onClick={onClick}
+      className={` ${withFlex ? "flex" : ""} ${hasBoldText ? "font-medium " : ""}  ${hasButtons ? "justify-center gap-2" : "flex-col items-center"}   ${isClickable ? "cursor-pointer hover:bg-slate-200  " : ""} whitespace-nowrap px-6 py-4 text-center  text-sm text-gray-900`}
     >
       {children}
     </td>
